760a3cc93b1aad62a5364cc2329c1b5d0c1e14b1
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-09 15:36:56 -0300

    Anotation @Repository

--- blog/src/main/java/com/menegheti/blog/interfaces/PostagemRepository.java
+++ blog/src/main/java/com/menegheti/blog/interfaces/PostagemRepository.java
@@ -1,5 +1,12 @@
 package com.menegheti.blog.interfaces;
 
-public interface PostagemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.menegheti.blog.entities.Postagem;
+
+@Repository
+public interface PostagemRepository extends JpaRepository<Postagem, Integer>{
+// com esta simples interface podemos dazer as quatro operações na classe Postagem
+//consulta, cadastro, edição, e exclusão
 }
