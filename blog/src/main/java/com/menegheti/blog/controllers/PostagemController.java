d2ae784c0d15677c00a219b6b8176d16e72bbc11
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-10 21:16:38 -0300

    Salvar uma postagem no banco (via POST) - PostagemController.java

---
+++ blog/src/main/java/com/menegheti/blog/controllers/PostagemController.java
@@ -0,0 +1,32 @@
+package com.menegheti.blog.controllers;
+
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.menegheti.blog.entities.Postagem;
+import com.menegheti.blog.services.PostagemService;
+
+
+
+@RestController
+@RequestMapping(value="/postagem")
+public class PostagemController {
+	
+	@Autowired
+	private PostagemService postagemService;
+	
+	@RequestMapping(method=RequestMethod.POST)
+	public ResponseEntity<Object> salvar(@RequestBody Postagem obj) {
+		obj = postagemService.salvar(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+}
