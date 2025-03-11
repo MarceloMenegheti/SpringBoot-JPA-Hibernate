1d58c0f5a130c87545986795a6fd1f7bb0977222
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-10 21:40:21 -0300

    buscar todas as postagens no banco (via GET) - PostagemController.java

--- blog/src/main/java/com/menegheti/blog/controllers/PostagemController.java
+++ blog/src/main/java/com/menegheti/blog/controllers/PostagemController.java
@@ -1,9 +1,11 @@
 package com.menegheti.blog.controllers;
 
 import java.net.URI;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,5 +30,11 @@ public class PostagemController {
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(method=RequestMethod.GET)
+	public ResponseEntity<List<Postagem>> buscarTodos() {
+		List<Postagem> list = postagemService.buscarTodos();
+		return ResponseEntity.ok().body(list);
 	}
 }
