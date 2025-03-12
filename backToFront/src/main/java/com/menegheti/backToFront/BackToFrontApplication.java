cd5e8c21cf82cef6dc858a34592ad520e0b61be0
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-11 22:16:43 -0300

    initial commit - backToFront

---
+++ backToFront/src/main/java/com/menegheti/backToFront/BackToFrontApplication.java
@@ -0,0 +1,13 @@
+package com.menegheti.backToFront;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackToFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackToFrontApplication.class, args);
+	}
+
+}
---
+++ backToFront/src/test/java/com/menegheti/backToFront/BackToFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menegheti.backToFront;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackToFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
