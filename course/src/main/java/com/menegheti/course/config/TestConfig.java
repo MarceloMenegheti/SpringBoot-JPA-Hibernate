be82b1020cf6db83258c301ff8230064e9a363f0
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-18 14:36:15 -0300

    Injecao de dependencia @Autowired UserRepository

--- course/src/main/java/com/menegheti/course/config/TestConfig.java
+++ course/src/main/java/com/menegheti/course/config/TestConfig.java
@@ -1,5 +1,18 @@
 package com.menegheti.course.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.menegheti.course.repositories.UserRepository;
+
+@Configuration
+@Profile("test")
 public class TestConfig {
+	
+	@Autowired
+	private UserRepository userRepository;
+
+
 
 }
