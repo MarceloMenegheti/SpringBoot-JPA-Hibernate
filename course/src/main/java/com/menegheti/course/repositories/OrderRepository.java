244ee67465b19dea9e63c5ca37eaa55e431f1292
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-18 16:17:42 -0300

    implementação OrderRepository, OrderService e OrderController

---
+++ course/src/main/java/com/menegheti/course/repositories/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.menegheti.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.menegheti.course.entities.Order;
+
+public interface OrderRepository extends JpaRepository<Order, Long>{
+
+}
