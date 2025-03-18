da4c841e42f445cf6a65ec9dc1b24b19247ed55f
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-18 14:35:47 -0300

    Interface UserRepository

--- course/src/main/java/com/menegheti/course/repositories/UserRepository.java
+++ course/src/main/java/com/menegheti/course/repositories/UserRepository.java
@@ -1,5 +1,9 @@
 package com.menegheti.course.repositories;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.menegheti.course.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
 
 }
