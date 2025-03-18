f1cad18a32b4444db92c38ad72688f809d6c5b21
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-18 16:10:57 -0300

    add mapeamento JPA entre User e Order e o nome tabela Tb_order

--- course/src/main/java/com/menegheti/course/entities/Order.java
+++ course/src/main/java/com/menegheti/course/entities/Order.java
@@ -4,13 +4,27 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_order")
 public class Order implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
 	
+	@ManyToOne
+	@JoinColumn(name = "client_id")
 	private User client;
 
 	public Order() {
