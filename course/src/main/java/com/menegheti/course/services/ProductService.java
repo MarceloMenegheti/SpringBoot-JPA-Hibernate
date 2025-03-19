6852b9d036f919ac6b44ee7886e11b14bf82435a
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-19 10:48:08 -0300

    implementando ProductService e ProductController

---
+++ course/src/main/java/com/menegheti/course/services/ProductService.java
@@ -0,0 +1,27 @@
+package com.menegheti.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.menegheti.course.entities.Product;
+import com.menegheti.course.repositories.ProductRepository;
+
+@Service
+public class ProductService {
+	
+	@Autowired
+	private ProductRepository repository;
+	
+	public List<Product> findAll(){
+		return repository.findAll();
+	}
+	
+	public Product findById(Long id) {
+		Optional<Product> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+}
