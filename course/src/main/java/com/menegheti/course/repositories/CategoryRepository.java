a1173d845ef5e4ecb297e26098d4446b8b939cbb
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-19 10:13:00 -0300

    adicionando CategoryRepository e popula o banco de dados

---
+++ course/src/main/java/com/menegheti/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.menegheti.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.menegheti.course.entities.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long>{
+
+}
