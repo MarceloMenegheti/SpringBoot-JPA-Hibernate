e1c6b0584abeca1452f3a1bc0c71cc8b6eee8873
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-19 10:32:41 -0300

    mapeamento jpa em Product

--- course/src/main/java/com/menegheti/course/entities/Product.java
+++ course/src/main/java/com/menegheti/course/entities/Product.java
@@ -6,10 +6,20 @@ import java.util.Locale.Category;
 import java.util.Objects;
 import java.util.Set;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="tb_product")
 public class Product implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-			
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String description;
