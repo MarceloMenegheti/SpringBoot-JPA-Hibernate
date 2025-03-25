3557a94bbd0096f67dbaa566cb08ab8e1283a719
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-03-24 22:33:24 -0300

    mapeamento jpa em Payment

--- course/src/main/java/com/menegheti/course/entities/Payment.java
+++ course/src/main/java/com/menegheti/course/entities/Payment.java
@@ -4,10 +4,20 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_payment")
 public class Payment implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private Instant moment;
 	
